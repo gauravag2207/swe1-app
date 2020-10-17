943fed45d51015bbf459df817e2adf757a0b47de
unknown <gauravag2207@gmail.com>
2020-10-17 16:38:13 -0500

    Reformated with 'black'

--- polls/admin.py
+++ polls/admin.py
@@ -8,16 +8,16 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
     ]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     inlines = [ChoiceInline]
-    
 
 
-admin.site.register(Question,QuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
